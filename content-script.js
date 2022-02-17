cf3c903da9cafcb9464ecc8103a5695af86920b5
Yi Hein Chai <chaiyihein@gmail.com>
2022-02-17 12:53:31 +0000

    First working prototype

---
+++ content-script.js
@@ -0,0 +1,79 @@
+setTimeout(function () {
+  // Your code here...
+  //do what you need here
+  document.querySelector(".transcript").click();
+  document.querySelector("#tooltips").innerHTML = `
+    <div style="width: 100vw; bottom: 50px; display: flex; flex-flow: row; justify-content: center; z-index: 3; position: fixed"">
+                  <div
+                    class="inline-subs"
+                    style="background-color: rgba(8, 8, 8, 0.75); font-size: 22px; display: flex; flex-flow: row; justify-content: center; color: rgb(255, 255, 255); padding: 0.25rem; border-radius: 0.25rem"
+                  >Welcome to Yi Hein's Inline Subs
+                  </div>
+                </div>
+    `;
+
+  function updateSubtitle() {
+    const width = document.querySelector(".screens").getBoundingClientRect().width;
+    document.querySelector("#tooltips").innerHTML = `
+                <div style="width: ${width}px; bottom: 50px; display: flex; flex-flow: row; justify-content: center; z-index: 3; position: fixed">
+                  <div
+                    class="inline-subs"
+                    draggable="true"
+                    style="background-color: rgba(8, 8, 8, 0.75); font-size: 22px; display: flex; flex-flow: row; justify-content: center; color: rgb(255, 255, 255); padding: 0.25rem 0.5rem 0.25rem 0.5rem; border-radius: 0.25rem"
+                  >
+                  </div>
+                </div>
+                `;
+    const text = document.querySelector(".highlight").textContent;
+    document.querySelector(".inline-subs").textContent = text;
+  }
+  try {
+    const observer = new MutationObserver(updateSubtitle);
+    observer.observe(document.querySelector(".transcript-cues"), {
+      attributes: true,
+      childList: true,
+      subtree: true,
+    });
+    document.querySelector(".transcript-panel").style.display = "none";
+    document.querySelector(".sidebar").style.flex = "0 0";
+
+    // Enable drag and drop
+    function dragstart_handler(ev) {
+      // Add the target element's id to the data transfer object
+      ev.dataTransfer.setData("text/plain", ev.target.id);
+      ev.dataTransfer.dropEffect = "move";
+    }
+    const dragableElement = document.querySelector(".inline-subs");
+    // Add the ondragstart event listener
+    dragableElement.addEventListener("dragstart", dragstart_handler);
+
+    // enable transcript panel
+    const openTranscriptButton = document.querySelector(".actions");
+    function showTranscript() {
+      document.querySelector(".transcript-panel").style = "";
+      document.querySelector(".sidebar").style.flex = "0 0 400px";
+      openTranscriptButton.innerHTML = `
+      <button class="close_transcript" style="background-color: #00aee4; color: #000f19; font: inherit; border: none;border-radius: 0.25rem; font-size: 13px">Hide</button>
+      `;
+      document.querySelector(".close_transcript").onclick = hideTranscript;
+    }
+    function hideTranscript() {
+      document.querySelector(".transcript-panel").style.display = "none";
+      document.querySelector(".sidebar").style.flex = "0 0";
+      openTranscriptButton.innerHTML = `
+        <button class="open_transcript" style="background-color: #00aee4; color: #000f19; font: inherit; border: none; border-radius: 0.25rem; font-size: 13px">Show</button>
+        `;
+      document.querySelector(".open_transcript").onclick = showTranscript;
+    }
+
+    openTranscriptButton.innerHTML = `
+      <button class="open_transcript" style="background-color: #00aee4; color: #000f19; font: inherit; border: none; border-radius: 0.25rem; font-size: 13px">Show</button>
+      `;
+    document.querySelector(".open_transcript").onclick = showTranscript;
+  } catch (error) {
+    console.error(error);
+    document.querySelector(
+      "#tooltips"
+    ).innerHTML = `<div class="inline-subs" style="width: 100vw; background-color: white; bottom: 50px; font-size: 22px; display: flex; flex-flow: row; justify-content: center; z-index: 3; position: fixed; color: red;"><p>ERROR: Refresh the page, open transcript panel, paste the code again<p></div>`;
+  }
+}, 2000);
